960484693719bb1fb24fc67d350436334b944f1d
heikok <heikok@03d4b741-bd0f-0410-913c-e9edee52428d>
2006-06-05 11:03:59 +0000

    makeing real shell scripts, (shebang, useage and args)
    git-svn-id: https://javaxdelta.svn.sourceforge.net/svnroot/javaxdelta/trunk/javaxdelta@11 03d4b741-bd0f-0410-913c-e9edee52428d

--- patch.sh
+++ patch.sh
@@ -1,3 +1,6 @@
-#
-
-java -cp ./classes: com.nothome.delta.GDiffPatcher source delta result
+#! /bin/sh
+if  [ "x$3" != "x" -a -x $2 ]; then
+    java -cp ./classes: com.nothome.delta.GDiffPatcher $1 $2 $3;
+else
+   echo "useage: java -cp ./classes com.nothome.delta.GDiffPatcher source delta result";
+fi
--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
-#
+#! /bin/sh
+if  [ "x$3" != "x" -a -x $2 ]; then
+    java -cp ./classes: com.nothome.delta.Delta $1 $2 $3;
+else
+   echo "useage: java -cp ./classes: com.nothome.delta.Delta source target delta";
+fi
 
-java -cp ./classes: com.nothome.delta.Delta source target delta
+
