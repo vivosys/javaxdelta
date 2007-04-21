4b4eea9f0d6beb35471034c2c3b4910389ddc51f
heikok <heikok@03d4b741-bd0f-0410-913c-e9edee52428d>
2007-04-21 19:01:50 +0000

    using bit-operations for speed-up
    git-svn-id: https://javaxdelta.svn.sourceforge.net/svnroot/javaxdelta/trunk/javaxdelta@17 03d4b741-bd0f-0410-913c-e9edee52428d

--- com/nothome/delta/BitArray.java
+++ com/nothome/delta/BitArray.java
@@ -14,14 +14,15 @@ package com.nothome.delta;
 public class BitArray {
     int[] implArray;
     int size;
+    final static int INT_SIZE = 32;
     /** Creates a new instance of BitArray */
     public BitArray(int size) {
-        int implSize = (int) (size / Integer.SIZE) + 1;
+        int implSize = (int) (size / INT_SIZE) + 1;
         implArray = new int[implSize];
     }
     public void set(int pos, boolean value) {
-        int implPos = (int) (pos / Integer.SIZE);
-        int bitMask = 1 << (pos & (Integer.SIZE - 1));            
+        int implPos = (int) (pos / INT_SIZE);
+        int bitMask = 1 << (pos & (INT_SIZE - 1));            
         if (value) {
             implArray[implPos] |= bitMask; // set true if true
         } else {
@@ -30,8 +31,8 @@ public class BitArray {
     }
     
     public boolean get(int pos) {
-        int implPos = (int) (pos / Integer.SIZE);
-        int bitMask = 1 << (pos & (Integer.SIZE - 1));
+        int implPos = (int) (pos / INT_SIZE);
+        int bitMask = 1 << (pos & (INT_SIZE - 1));
         return (implArray[implPos] & bitMask) != 0;
     }
     
